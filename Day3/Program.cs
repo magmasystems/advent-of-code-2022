63fedc6c3dbaf11e1a8acc42767c317bf6b91859
magmasystems <magmasystems@gmail.com>
2022-12-03 09:42:54 -0500

    Cleanup

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -9,23 +9,26 @@ namespace AdventOfCode2022
         private static void Main(string[] args)
         {
             // This is part 1
+            var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
 
             var sumOfPriorities = 0;
-
-            var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
             foreach (var line in input)
             {
                 var halves = new[] { line[..(line.Length / 2)], line[(line.Length / 2)..] };
 
-                var map = halves[0].CreateCharMap();
-                foreach (var chIndex in halves[1].Select(CharToIndex))
+                var maps = new BitArray[2];
+                for (var i = 0; i < maps.Length; i++)
                 {
-                    if (map[chIndex])
-                    {
-                        sumOfPriorities += chIndex + 1;
-                        break;
-                    }
+                    maps[i] = halves[i].CreateCharMap();
                 }
+
+                // Note: I probably could have used a single map in the solution, but I like and'ing chains of maps
+                var resultMap = maps.And();
+
+                // Find the first entry that is non-zero
+                var idxNonZero = resultMap.FindIndexOf(element => element);
+                if (idxNonZero >= 0)
+                    sumOfPriorities += idxNonZero + 1;
             }
 
             Console.WriteLine($"Part 1: The sum of priorities is {sumOfPriorities}"); // 8401
@@ -36,12 +39,12 @@ namespace AdventOfCode2022
             // For each set of 3 lines, we need to find the common element in each line.
 
             sumOfPriorities = 0;
-            for (var idxLine = 0; idxLine < input.Length && !string.IsNullOrEmpty(input[idxLine]); idxLine += 3)
+            for (var idxLine = 0; idxLine < input.Length; idxLine += 3)
             {
                 var maps = new BitArray[3];
-                for (var idxElf = 0; idxElf < 3; idxElf++)
+                for (var i = 0; i < maps.Length; i++)
                 {
-                    maps[idxElf] = input[idxLine + idxElf].CreateCharMap();
+                    maps[i] = input[idxLine + i].CreateCharMap();
                 }
 
                 // Note: I probably could have used a single map in the solution, but I like and'ing chains of maps
