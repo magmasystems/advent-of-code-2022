4b3f19beba678f9cceb7db048ab3cf12038e4242
magmasystems <magmasystems@gmail.com>
2022-12-08 09:27:45 -0500

    Day8

--- Day8/ArrayUtils.cs
+++ Day8/ArrayUtils.cs
@@ -2,21 +2,25 @@ namespace AdventOfCode2022;
 
 public static class ArrayUtils
 {
-    public static IEnumerable<T> SliceRow<T>(this T[,] array, int row, int colToExclude = -1)
+    public static IEnumerable<T> SliceRow<T>(this T[,] array, int row, int startCol, int endCol = -1)
     {
-        for (var col = 0; col < array.GetLength(1); col++)
+        if (endCol == -1)
+            endCol = array.GetLength(1);
+        
+        for (var col = startCol; col < endCol; col++)
         {
-            if (colToExclude == -1 || col != colToExclude)
-                yield return array[row, col];
+            yield return array[row, col];
         }
     }
 
-    public static IEnumerable<T> SliceColumn<T>(this T[,] array, int col, int rowToExclude = -1)
+    public static IEnumerable<T> SliceColumn<T>(this T[,] array, int col, int startRow, int endRow = -1)
     {
-        for (var row = 0; row < array.GetLength(0); row++)
+        if (endRow == -1)
+            endRow = array.GetLength(0);
+        
+        for (var row = startRow; row < endRow; row++)
         {
-            if (rowToExclude == -1 || row != rowToExclude)
-                yield return array[row, col];
+            yield return array[row, col];
         }
     }
 }
--- Day8/Program.cs
+++ Day8/Program.cs
@@ -7,7 +7,7 @@
             var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
             var grid = new int[input.Length,input[0].Length];
             
-            // Part 1
+            // Parse the input
             for (var row = 0; row < input.Length; row++)
             {
                 var line = input[row];
@@ -16,10 +16,12 @@
                     grid[row, col] = line[col] - '0';
                 }
             }
-
+            
             var numRows = grid.GetLength(0);
             var numCols = grid.GetLength(1);
-
+            
+            // Part 1
+            
             // All trees at the edge of the grid are visible
             var numVisible = input.Length * 2 + (input[0].Length - 2) * 2;
 
@@ -27,9 +29,7 @@
             {
                 for (var col = 1; col < numCols - 1; col++)
                 {
-                    var isVisible = IsTreeVisible(grid, row, col, numRows, numCols);
-                    if (isVisible)
-                        numVisible++;
+                    numVisible += IsTreeVisible(grid, row, col) ? 1 : 0;
                 }
             }
 
@@ -48,40 +48,17 @@
             Console.WriteLine($"Part 2: The max viewing area is {scenicScores.Max()}");         // 527340
         }
 
-        private static bool IsTreeVisible(int[,] grid, int row, int col, int numRows, int numCols)
+        private static bool IsTreeVisible(int[,] grid, int row, int col)
         {
             var tree = grid[row,col];
-
-            // Check up - it's visible if all trees are smaller
-            var isVisible = true;
-            for (var r = 0; r < row && isVisible;  r++)
-                if (grid[r, col] >= tree)
-                    isVisible = false;
-            if (isVisible)
-                return true;
             
-            // Check down
-            isVisible = true;
-            for (var r = row+1; r < numRows && isVisible;  r++)
-                if (grid[r, col] >= tree)
-                    isVisible = false;
-            if (isVisible)
+            if (grid.SliceColumn(col, 0, row).Max() < tree)
                 return true;
-            
-            // Check left
-            isVisible = true;
-            for (var c = 0; c < col && isVisible;  c++)
-                if (grid[row, c] >= tree)
-                    isVisible = false;
-            if (isVisible)
+            if (grid.SliceColumn(col, row+1).Max() < tree)
                 return true;
-            
-            // Check right
-            isVisible = true;
-            for (var c = col+1; c < numCols && isVisible;  c++)
-                if (grid[row, c] >= tree)
-                    isVisible = false;
-            if (isVisible)
+            if (grid.SliceRow(row, 0, col).Max() < tree)
+                return true;
+            if (grid.SliceRow(row, col+1).Max() < tree)
                 return true;
 
             return false;
