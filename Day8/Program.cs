9def5b1b98c016ec2f5e4aabf37740eda694fa21
magmasystems <magmasystems@gmail.com>
2022-12-08 08:51:49 -0500

    Day8

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -74,5 +74,23 @@
 
             return false;
         }
+        
+        public static IEnumerable<T> SliceRow<T>(this T[,] array, int row, int colToExclude = -1)
+        {
+            for (var col = 0; col < array.GetLength(1); col++)
+            {
+                if (colToExclude == -1 || col != colToExclude)
+                    yield return array[row, col];
+            }
+        }
+        
+        public static IEnumerable<T> SliceColumn<T>(this T[,] array, int col, int rowToExclude = -1)
+        {
+            for (var row = 0; row < array.GetLength(0); row++)
+            {
+                if (rowToExclude == -1 || row != rowToExclude)
+                    yield return array[row, col];
+            }
+        }
     }
 }
