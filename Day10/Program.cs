32dc74f82e610fe21133bfde1f701377ab7b7e59
magmasystems <magmasystems@gmail.com>
2022-12-10 11:41:40 -0500

    Day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,15 +1,17 @@
-﻿namespace AdventOfCode2022
+﻿using System.Collections;
+
+namespace AdventOfCode2022
 {
     internal static class Program
     {
-        private struct Accumulator
+        private struct Instruction
         {
             public int Cycle { get; set; }
-            public int V { get; set; }
+            public int Value { get; set; }
 
             public override string ToString()
             {
-                return $"{nameof(Cycle)}: {Cycle}, {nameof(V)}: {V}";
+                return $"{nameof(Cycle)}: {Cycle}, {nameof(Value)}: {Value}";
             }
         }
         
@@ -20,7 +22,7 @@
             // Part 1
             var cycle = 0;
             var idxInstruction = 0;
-            var cycles = new Accumulator[input.Length];
+            var instructions = new Instruction[input.Length];
             
             foreach (var line in input)
             {
@@ -28,12 +30,12 @@
                 switch (parts[0].ToLower())
                 {
                     case "noop":
-                        cycles[idxInstruction].Cycle = ++cycle;
+                        instructions[idxInstruction].Cycle = ++cycle;
                         break;
                     case "addx":
                         cycle += 2;
-                        cycles[idxInstruction].Cycle = cycle;
-                        cycles[idxInstruction].V = Convert.ToInt32(parts[1]);
+                        instructions[idxInstruction].Cycle = cycle;
+                        instructions[idxInstruction].Value = Convert.ToInt32(parts[1]);
                         break;
                 }
                 idxInstruction++;
@@ -48,7 +50,7 @@
             
             foreach (var cycleToTest in cyclesToTest)
             {
-                register += cycles.Where(c => c.Cycle >= prevCycleToTest && c.Cycle < cycleToTest).Sum(C => C.V);
+                register += instructions.Where(c => c.Cycle >= prevCycleToTest && c.Cycle < cycleToTest).Sum(C => C.Value);
                 sumOfSignalStrengths += register * cycleToTest;
                 prevCycleToTest = cycleToTest;
                 // Console.WriteLine($"Cycle: {testPoint}, Register: {register}, Accumulator: {acc}");
@@ -57,6 +59,43 @@
             Console.WriteLine($"Part 1: The sum is {sumOfSignalStrengths}"); // 15140
             
             // Part 2
+            const int MAXCYCLES = 240;
+            const int NUMCOLUMNS = 40;
+            
+            var crt = new BitArray(MAXCYCLES, false);
+            var spritePosition = 1;
+            idxInstruction = 0;
+            register = 1;
+            
+            for (cycle = 1; cycle <= MAXCYCLES; cycle++)
+            {
+                // If the column that we are drawing in overlaps with the 3-column sprite, then turn on the pixel in the CRT
+                var col = (cycle - 1) % NUMCOLUMNS;
+                if (col >= spritePosition - 1 && col <= spritePosition + 1)
+                {
+                    crt[cycle - 1] = true;
+                }
+                
+                if (cycle >= instructions[idxInstruction].Cycle)
+                {
+                    register += instructions[idxInstruction].Value;
+                    spritePosition = register;
+                    idxInstruction++;
+                }
+            }
+            
+            Console.WriteLine("Part 2: The letters are");
+
+            for (var i = 0; i < 240; i++)
+            {
+                if (i % 40 == 0 && i > 0)
+                {
+                    Console.WriteLine();
+                }
+                if (i % 5 == 0)
+                    Console.Write("     ");
+                Console.Write(crt[i] ? "█" : '.');
+            }
         }
     }
 }
