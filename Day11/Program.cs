5cf4260c9aca93468f4d7e7154bffb5332f8de89
magmasystems <magmasystems@gmail.com>
2022-12-10 17:51:26 -0500

    Renamed the single-digit projects so that they have a leading zero.

---
+++ Day11/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2022
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
+
+            // Part 1
+            foreach (var line in input)
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+            
+            Console.WriteLine($"Part 1: ");
+        }
+    }
+}
